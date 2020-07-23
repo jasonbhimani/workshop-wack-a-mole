3568c99de1195e3eaa7a2290968c42f316de42dc
Jason Bhimani <jasonbhimani10@gmail.com>
2020-07-23 10:55:10 -0400

    Final Whack-A-Mole

--- index.js
+++ index.js
@@ -2,6 +2,26 @@
 
 let score = 0;
 var scoreDisplay = document.getElementById('score');
+var holes = Array.from(document.getElementsByClassName('hole'))
+const gameArea = document.getElementById('whack-a-mole')
 
 console.log(scoreDisplay);
-console.log("line 7", score)
+console.log("line 7", score);
+
+function getRandom(){
+    return Math.floor(Math.random()*(9));
+};
+
+
+setInterval(function(){
+let randomHoleIndex = Math.floor(getRandom(0, holes.length))
+holes[randomHoleIndex].classList.toggle('mole')
+}, 1000);
+
+gameArea.addEventListener('click', function(click){
+if (click.target.matches('.mole')){
+    score++
+    scoreDisplay.innerHTML = score
+    click.target.classList.toggle('mole')
+}
+});
